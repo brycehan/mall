32a12bb3060a0a40f69da3031d4d02c4533eae6e
韩世强 <brycehan@163.com>
2021-01-20 05:20:38 +0800

    Initial commit

---
+++ member/src/test/java/com/brycehan/mall/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brycehan.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
